9db9249984f3045d01a9acef117e3b0926705d9c
PAVAN903 <navap903@gmail.com>
2023-01-14 15:04:15 +0530

    valiadate last name

--- src/com/exceptionuser/Firstname.java
+++ src/com/exceptionuser/Firstname.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class Firstname {
 	static String fName;
+	static String lName;
 	
 	static String fName() {
 		Scanner sc=new Scanner(System.in);
@@ -12,18 +13,27 @@ public class Firstname {
 		fName=sc.next();
 		return fName;
 	}
+	static String lName() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter last name ");
+		lName=sc.next();
+		return lName;
+	}
+	
 	static void Print(String fName) {
-		boolean pattern=Pattern.matches("[A-Z]{1}[a-z]{3,}", fName);
+		boolean pattern=Pattern.matches("[A-Z]{1}[a-z]{3,}", lName);
 		if(pattern==true) {
-			System.out.println(" "+fName+ "is valid name");
+			System.out.println(" "+lName+ "is valid name");
 		}else {
-			System.out.println(" "+fName+ "is not valid name");
+			System.out.println(" "+lName+ "is not valid name");
 	}
 	}
      public static void main(String[] args) {
     	 String fName=fName();
-    	 System.out.println("first name is:->");
-		Print(fName);
+    	 String lName=lName();
+    	 System.out.println("last name is:->");
+    	 Print(fName);
+		Print(lName);
 
 	}
 
