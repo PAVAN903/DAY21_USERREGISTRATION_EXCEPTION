caa6165fb8c6bddad67602c368c573ff716dc287
PAVAN903 <navap903@gmail.com>
2023-01-14 16:53:46 +0530

    validating upper case  password

--- src/com/exceptionuser/Firstname.java
+++ src/com/exceptionuser/Firstname.java
@@ -4,61 +4,70 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Firstname {
-	//static String fName;
-	//static String lName;
-	//static String mail;
-	//static String mNumber;
+	static String fName;
+	static String lName;
+	static String mail;
+    static String mNumber;
 	static String password;
+	static String uPASSWORD;
 	
-	//static String fName() {
-		//Scanner sc=new Scanner(System.in);
-		//System.out.println("enter first name");
-		//fName=sc.next();
-		//return fName;
-	//}
-	//static String lName() {
-		//Scanner sc=new Scanner(System.in);
-		//System.out.println("enter last name ");
-		//lName=sc.next();
-		//return lName;
-	//}
-	//static String mail() {
-		//Scanner sc=new Scanner(System.in);
-		//System.out.println("enter mail address");
-		//mail=sc.next();
-		//return  mail;
-	//}
-	//static String mNumber() {
-		//Scanner sc=new Scanner(System.in);
-		//System.out.println("enter mobile number");
-	     //mNumber=sc.nextLine();
-		//return  mNumber;
-	//}
+	static String fName() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first name");
+		fName=sc.next();
+		return fName;
+	}
+	static String lName() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter last name ");
+		lName=sc.next();
+		return lName;
+	}
+	static String mail() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter mail address");
+		mail=sc.next();
+	return  mail;
+	}
+	static String mNumber() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter mobile number");
+	     mNumber=sc.nextLine();
+		return  mNumber;
+	}
 	static String password() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter password");
 	     password=sc.next();
 		return password;
 	}
-	static void Print(String password) {
-		boolean pattern=Pattern.matches("[a-z]{8,}", password);
+	static String uPASSWORD() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter password");
+	     uPASSWORD=sc.next();
+		return uPASSWORD;
+	}
+	static void Print(String uPASSWORD) {
+		boolean pattern=Pattern.matches("[A-Z]{1}[a-z]{8,}", uPASSWORD);
 		if(pattern==true) {
-			System.out.println(" "+password+ " is valid password number");
+			System.out.println(" "+uPASSWORD+ " is valid password number");
 		}else {
-			System.out.println(" "+password+ " is not valid password number");
+			System.out.println(" "+uPASSWORD+ " is not valid password number");
 	}
 	}
      public static void main(String[] args) {
-    	// String fName=fName();
-    	 //String lName=lName();
-    	 //String mail=mail();
-    	// String mNumber=mNumber();
+    	 String fName=fName();
+    	 String lName=lName();
+    	 String mail=mail();
+    	 String mNumber=mNumber();
     	 String password=password();
+    	 String password1=uPASSWORD();
     	 System.out.println("password  is:->");
-    	 //Print(fName);
-		 //Print(lName);
-    	 // Print(mail);
-    	 Print(password);
+    	 Print(fName);
+		 Print(lName);
+    	 Print(mail);
+    	 Print(password1);
+    	 Print(password1);
     	
 
 	}
---
+++ src/com/exceptionuser/Main.java
@@ -0,0 +1,30 @@
+package com.exceptionuser;
+
+public class Main {
+	 public static void main(String[] args) {
+	    	// String fName=fName();
+	    	 //String lName=lName();
+	    	 //String mail=mail();
+	    	// String mNumber=mNumber();
+	    	 String password=password();
+	    	 System.out.println("password  is:->");
+	    	 //Print(fName);
+			 //Print(lName);
+	    	 // Print(mail);
+	    	    Print(password);
+	    	
+
+		}
+
+	private static String password() {
+		return password();
+	}
+
+	private static void Print(String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	}
+
+
