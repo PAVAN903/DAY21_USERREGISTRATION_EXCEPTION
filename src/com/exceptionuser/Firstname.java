8c78cc6002cd61a945780ad81de487e8f5859322
PAVAN903 <navap903@gmail.com>
2023-01-14 15:45:13 +0530

    validate mail address

--- src/com/exceptionuser/Firstname.java
+++ src/com/exceptionuser/Firstname.java
@@ -4,36 +4,45 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Firstname {
-	static String fName;
-	static String lName;
+	//static String fName;
+	//static String lName;
+	static String mail;
 	
-	static String fName() {
+	//static String fName() {
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("enter first name");
+		//fName=sc.next();
+		//return fName;
+	//}
+	//static String lName() {
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("enter last name ");
+		//lName=sc.next();
+		//return lName;
+	//}
+	static String mail() {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter first name");
-		fName=sc.next();
-		return fName;
-	}
-	static String lName() {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter last name ");
-		lName=sc.next();
-		return lName;
+		System.out.println("enter mail address");
+		mail=sc.next();
+		return  mail;
 	}
 	
 	static void Print(String fName) {
-		boolean pattern=Pattern.matches("[A-Z]{1}[a-z]{3,}", lName);
+		boolean pattern=Pattern.matches("abc.[a-z]+@bl.co.[a-z]+gmail", mail);
 		if(pattern==true) {
-			System.out.println(" "+lName+ "is valid name");
+			System.out.println(" "+mail+ " is valid mail");
 		}else {
-			System.out.println(" "+lName+ "is not valid name");
+			System.out.println(" "+mail+ " is not valid mail");
 	}
 	}
      public static void main(String[] args) {
-    	 String fName=fName();
-    	 String lName=lName();
-    	 System.out.println("last name is:->");
-    	 Print(fName);
-		Print(lName);
+    	// String fName=fName();
+    	 //String lName=lName();
+    	 String mail=mail();
+    	 System.out.println("mail address is:->");
+    	 //Print(fName);
+		 //Print(lName);
+    	  Print(mail);
 
 	}
 
